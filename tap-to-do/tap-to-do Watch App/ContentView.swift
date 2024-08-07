0eff845c62161268ecf18eefdc45804ed360c8ca
Eunbin Kwon <kwoneunbini@gmail.com>
2024-08-07 20:20:19 +0900

    chore: init project

---
+++ tap-to-do/tap-to-do Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  tap-to-do Watch App
+//
+//  Created by 권은빈 on 2024.08.07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ tap-to-do/tap-to-do Watch App/tap_to_doApp.swift
@@ -0,0 +1,17 @@
+//
+//  tap_to_doApp.swift
+//  tap-to-do Watch App
+//
+//  Created by 권은빈 on 2024.08.07.
+//
+
+import SwiftUI
+
+@main
+struct tap_to_do_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
